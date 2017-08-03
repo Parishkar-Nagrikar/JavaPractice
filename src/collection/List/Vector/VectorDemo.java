4afe764a812691173c3c65acc2979549b97dae2a
p-Star <p4parish@gmail.com>
2017-08-03 10:07:24 +0530

    "Sampler Java Programs"

---
+++ src/collection/List/Vector/VectorDemo.java
@@ -0,0 +1,15 @@
+package collection.List.Vector;
+
+/**
+ * Created by p-Star on 7/20/2017.
+ */
+public class VectorDemo
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
 	// write your 0t0code here
+        List list =new ArrayList<>();
 
+        list.add("10");
+        list.add("11");
+        list.add("12");
+
+       // System.out.println(list);
+
+        String str="HelloWorld";
+
+        String rev=" ";
+
+        System.out.println(str.length());
+
+        for (int i=str.length()-1;i>=0;i--)
+        {
+            System.out.println( rev);
+            rev.charAt(i);
+        }
+        System.out.println(rev);
 
 
     }
